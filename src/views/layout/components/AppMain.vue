9ccf49992e190e0b6ab57769a861290b73b83514
Jacknode <729374683@qq.com>
2018-11-20 09:29:50 +0800

    lb

---
+++ src/views/layout/components/AppMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="app-main" id="app-main" @click.stop="appMain">
+    <transition name="fade" mode="out-in">
+      <!-- <router-view :key="key"></router-view> -->
+      <router-view></router-view>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  computed: {
+    // key() {
+    //   return this.$route.name !== undefined ? this.$route.name + +new Date() : this.$route + +new Date()
+    // }
+  },
+  methods:{
+    appMain(){
+      console.log(this.$route)
+    }
+  }
+
+}
+</script>
